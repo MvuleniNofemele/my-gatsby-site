19acd187a43f7947b7f16ee1051ef09464c49976
ABMNAET <Mvuleni.Nofemele@absa.africa>
2024-02-19 11:56:40 +0200

    Added posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,19 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site Mvuleni",
   },
-  plugins: [],
-}
+  plugins: [
+    // ...
+  ],
+};
+
+
+/*module.exports = {
+  siteMetadata: {
+    title: "My First Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};*/
